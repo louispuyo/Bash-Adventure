4551c69f63440e7d77ec2b07a1f59f20510432f9
XLofi <germadejulio@gmail.com>
2021-11-29 14:47:28 -0400

    Headers v0.3 - Ajout monde.h

--- src/julio/testJ.c
+++ src/julio/testJ.c
@@ -95,51 +95,12 @@ void plat(){
 
         system("PAUSE");   // Le programme se met en pause
 }
-void men(){
-    int fin;
 
-    fin = 0;
-    while(!fin)
-    {
-        int c;
-
-        /* affichage menu */
-        printf("1.Nouvelle Partie 1\n"
-               "2.Charger Partie 2\n"
-               "3.Quitter\n");
-
-        c = getchar();
-
-        /* suppression des caracteres dans stdin */
-        if(c != '\n' && c != EOF)
-        {
-            int d;
-            while((d = getchar()) != '\n' && d != EOF);
-        }
-
-        switch(c)
-        {
-            case '1':
-                printf("Nouvelle Partie\n");
-                break;
-
-            case '2':
-                printf("Charger Partie\n");
-                break;
-
-            case '3':
-                printf("Quitter\n");
-                break;
-
-            default:
-                printf("Choix errone\n");
-        }
-    }
-}
 //---------------------------------------------------------------------------//
 int main() {
     printf("Hello, World!\n");
     initialisation();
+    //plat();
     return 0;
 }
 
